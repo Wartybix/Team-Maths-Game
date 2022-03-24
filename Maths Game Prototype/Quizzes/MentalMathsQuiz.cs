5d17c17c6e83036ed706de428e23172cb80af3ff
L Turnbull <l-turnbull@outlook.com>
2022-03-24 11:26:30 +0000

    Added Mental Maths Quiz child class.

---
+++ Maths Game Prototype/Quizzes/MentalMathsQuiz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths_Game_Prototype
+{
+    internal class MentalMathsQuiz : Quiz
+    {
+        public MentalMathsQuiz(string name) : base(name)
+        {
+        }
+
+        public override void NewGame()
+        {
+            base.NewGame();
+
+            foreach (Question question in Questions)
+            {
+                int x = Randoms.Next(0, 10);
+                int y = Randoms.Next(0, 10);
+
+                question.QuestionVariables.Add("x", x);
+                question.QuestionVariables.Add("y", y);
+
+                question.ExpectedAnswer.Add("ans", (x + y).ToString());
+            }
+        }
+
+        public override void DisplayQuestion()
+        {
+            base.DisplayQuestion();
+
+
+        }
+    }
+}
