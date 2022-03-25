24a13810ab7870295375428752507442f3c6f3db
L Turnbull <l-turnbull@outlook.com>
2022-03-25 08:47:16 +0000

    Added Operator and Operators classes to hold operator ('+', '-', '×', '÷') symbols and their respective in-game colours.

---
+++ Maths Game Prototype/Operator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Brush = System.Drawing.Brush;
+
+namespace Maths_Game_Prototype
+{
+    internal class Operator
+    {
+        public char Symbol { get; }
+        public SolidColorBrush Colour { get; }
+        public SolidColorBrush LightColour { get; }
+
+        public Operator(char symbol, SolidColorBrush colour, SolidColorBrush lightColour)
+        {
+            Symbol = symbol;
+            Colour = colour;
+            LightColour = lightColour;
+        }
+    }
+}
---
+++ Maths Game Prototype/Operators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Maths_Game_Prototype
+{
+    internal static class Operators
+    {
+        public static Operator Plus =>
+            new Operator('+', new SolidColorBrush(Colors.DarkOrange), 
+                new SolidColorBrush(Colors.Wheat));
+
+        public static Operator Minus =>
+            new Operator('-', new SolidColorBrush(Colors.MediumPurple),
+                new SolidColorBrush(Colors.Plum));
+
+    }
+}
